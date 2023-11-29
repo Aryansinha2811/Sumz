58b1c9a5a19ffd3adc641e718499c00838d462d9
Ansalpandey <pandeyansal77@gmail.com>
2023-11-29 19:04:21 +0530

    Update Hero component text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,11 +21,10 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='blue_gradient '>OpenAI GPT-4</span>
+        <span className='blue_gradient '>OpenAI's GPT-4</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+      Streamline Your Understanding with Summize: Unleashing Clarity by Condensing Complexity!
       </h2>
     </header>
   );
