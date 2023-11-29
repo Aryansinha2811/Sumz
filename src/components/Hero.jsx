d0d722b8ce977516832844c1722e97b6635c47d1
Ansalpandey <pandeyansal77@gmail.com>
2023-11-29 18:59:31 +0530

    GitHub error resolved, minor UI/UX improvements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Ansalpandey/IndustrialTrainingProject.git", "_blank")
           }
           className='black_btn'
         >
