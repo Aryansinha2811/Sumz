9eec770e896132a4bcc762c75e52bdf365684e15
Ankit kumar <125955449+Ankitkumar1100@users.noreply.github.com>
2023-12-06 19:21:14 +0530

    Push it also

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-vitejs.dev/config/
-https: export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
 });
